3ef972318a9bcd343603af62642c3cc11ac3e208
Vivek Doshi <vivek.doshi@apisero.com>
2022-04-17 17:10:30 +0530

    db

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
 const app = express();
-const url = "mongodb+srv://Vivek:Vivek123@woof.rtqxn.mongodb.net/Woof?retryWrites=true&w=majority"
+const url = "mongodb+srv://Vivek:Vivek123@woof.rtqxn.mongodb.net/Woof?directConnection=true"
 
 app.use(express.json());
 app.use(cors());
